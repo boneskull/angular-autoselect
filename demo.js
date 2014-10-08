663303b4487f50094434252d4f5b7a7df4c21277
Christopher Hiller <chiller@badwing.com>
2014-10-07 21:28:40 -0700

    Updates

--- demo.js
+++ demo.js
@@ -8,33 +8,33 @@
     {
       id: 'auto-select-all-text',
       title: 'Auto-Select All Text',
-      template: '<textarea ng-model="data.foo" auto-select></textarea>',
+      html: '<textarea ng-model="data.pomo_nonsense" auto-select></textarea>',
       description: 'This example auto-selects all text in a field upon model update.'
     },
     {
       id: 'auto-select-all-text-once',
       title: 'Auto-Select All Text (Once)',
-      template: '<textarea ng-model="data.foo" auto-select="{once: true}"></textarea>',
+      html: '<textarea ng-model="data.pomo_nonsense" auto-select="{once: true}"></textarea>',
       description: 'This example auto-selects all text in a field upon model update, but will only do this the first time the input field receives a value. Resetting will have no effect on the selection until the markup is removed and re-added to the DOM.'
     },
     {
       id: 'auto-select-range',
       title: 'Auto-Select Range',
-      template: '<textarea ng-model="data.foo" auto-select="{start: 10, end: 20}">',
+      html: '<textarea ng-model="data.pomo_nonsense" auto-select="{start: 10, end: 20}">',
       description: 'This example shows how a portion of text can be selected based on character index.'
     },
     {
       id: 'auto-select-substring',
       title: 'Auto-Select Substring',
-      template: '<textarea ng-model="data.foo" auto-select="{match: \'thematizes\'}">',
+      html: '<textarea ng-model="data.pomo_nonsense" auto-select="{match: \'thematizes\'}">',
       description: 'This example shows how a substring can be selected.'
     },
     {
       id: 'auto-select-regexp',
       title: 'Auto-Select Regular Expression',
-      template: '<textarea ng-model="data.foo" auto-select="{match: match}">',
+      html: '<textarea ng-model="data.pomo_nonsense" auto-select="{match: data.regex}">',
       description: 'This example shows how a <code>RegExp</code> can be used to perform a selection.  Note that you cannot declare a <code>RegExp</code> object within an AngularJS expression.',
-      js: '$scope.match = /(of\\s.+\\sboundaries|eht)/;'
+      js: '$scope.data = {regex: /(of\\s.+\\sboundaries|eht)/};'
     }
   ];
 
@@ -43,7 +43,7 @@
    */
   var run = function run($templateCache) {
     angular.forEach(examples, function (example) {
-      $templateCache.put(example.id + '-template', example.template);
+      $templateCache.put(example.id + '-template', example.html);
       $templateCache.put(example.id + '-description', example.description);
     });
   };
@@ -53,26 +53,25 @@
    * Controls a singular example
    */
   var ExampleCtrl = function ExampleCtrl($scope, $timeout) {
-    var foo = 'The fallacy of disciplinary boundaries thematizes the figuralization of civil society.';
+    var pomo_nonsense = 'The fallacy of disciplinary boundaries thematizes the figuralization of civil society.',
+      regex = /(of\s.+\sboundaries|eht)/;
 
     $scope.reset = function reset() {
-      delete $scope.foo;
+      delete $scope.data.pomo_nonsense;
       $timeout(function () {
-        $scope.foo = foo;
-        $scope.$digest();
+        $scope.data.pomo_nonsense = pomo_nonsense;
       });
     };
 
     $scope.reverse = function reverse() {
-      var arr = Array.prototype.slice.call($scope.data.foo);
+      var arr = Array.prototype.slice.call($scope.data.pomo_nonsense);
       arr.reverse();
-      $scope.data.foo = arr.join('');
+      $scope.data.pomo_nonsense = arr.join('');
     };
 
-    $scope.match = /(of\s.+\sboundaries|eht)/;
-
     $scope.data = {
-      foo: foo
+      regex: regex,
+      pomo_nonsense: pomo_nonsense
     };
   };
 
@@ -93,25 +92,45 @@
   MainCtrl.$inject = ['$scope', '$location', '$anchorScroll'];
 
   /**
-   * Highlights code once it enters the DOM
+   * Renders source code with highlight.js
+   * @example
+   * $scope.foo = "var bar = 'baz';";
+   * // <source ng-model="foo" lang="js"></source>
    */
-  var highlight = function highlight($window) {
+  var code = function code($window) {
     return {
-      link: function link(scope, element, attrs) {
-        var code = scope.$eval(attrs.highlight);
-        if (code) {
-          element.children('code').text(code);
-          $window.hljs.highlightBlock(element[0]);
+      restrict: 'E',
+      require: '?ngModel',
+      /**
+       *
+       * @param {$rootScope.Scope} scope Element Scope
+       * @param {angular.element} element AngularJS element
+       * @param {$compile.directive.Attributes} attrs AngularJS Attributes object
+       * @param {NgModelController} ngModel ngModel controller object
+       */
+      link: function link(scope, element, attrs, ngModel) {
+        var pre;
+        if (!ngModel) {
+          return;
         }
+
+        pre = element.wrap('<pre></pre>');
+        element.addClass(attrs.lang);
+
+        ngModel.$render = function $render() {
+          element.text(ngModel.$viewValue);
+          $window.hljs.highlightBlock(pre[0]);
+        };
+
       }
     };
   };
-  highlight.$inject = ['$window'];
+  code.$inject = ['$window'];
 
-  angular.module('demo', ['badwing.autoselect'])
+  angular.module('demo', ['badwing.autoselect', 'picardy.fontawesome'])
     .run(run)
     .controller('MainCtrl', MainCtrl)
     .controller('ExampleCtrl', ExampleCtrl)
-    .directive('highlight', highlight);
+    .directive('code', code);
 
 })(window.angular);
---
+++ support/angular-autoselect/README.md
@@ -0,0 +1,53 @@
+# angular-autoselect
+
+AngularJS directive(s) to automatically select text within input fields.
+
+## [Demo](http://boneskull.github.io/angular-autoselect)
+ 
+## Usage
+
+Include this module:
+
+```js
+angular.module('myModule', ['badwing.autoselect']);
+```
+
+Use the `autoSelect` directive:
+
+```html
+<input type="text" ng-model="foo" auto-select="options"/>
+```
+
+Where `options` is an object with the following properties:
+
+- **once** `{boolean}`: If true, only auto-select once.  Default `false`.
+- **start** `{number}`: If set, begin the selection at this index within the input field's value.  Default `0`.
+- **end** `{number}`: If set, end the selection at this index within the input field's value.  Default is the length of the value.
+- **match** `{(string|RegExp)}`: If a `string`, select only the substring, if present.  If a `RegExp`, then select all the text matched by the regular expression.  If the directive cannot find the substring or the regular expression, it will not attempt to select text.
+
+The `autoSelect` directive requires the `ngModel` directive.
+
+> Note: Not all input fields are supported by all browsers.  [Read about support](https://html.spec.whatwg.org/multipage/forms.html#do-not-apply).
+
+## Installation
+
+```shell
+$ npm install angular-autoselect
+```
+
+or
+
+```shell
+$ bower install angular-autoselect
+```
+
+**angular-autoselect** is a [UMD](https://github.com/umdjs/umd) module.
+
+## Author
+
+[Christopher Hiller](http://boneskull.github.io)
+
+## License
+
+MIT
+
